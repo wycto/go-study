0d938217cfb516512173798b6f7b99aebb71df93
wycto <294287600@qq.com>
2020-10-29 21:06:33 +0800

    学习调试

---
+++ example/MakeOrNew.go
@@ -0,0 +1,8 @@
+package example
+
+type MakeOrNew struct {
+}
+
+func (receiver *MakeOrNew) Make() {
+
+}
--- main.go
+++ main.go
@@ -1,16 +1,14 @@
 package main
 
-import (
-	"fmt"
-	"study/test/math"
-)
-
 func main() {
-	c := math.Add(12, 23)
+	/*c := math.Add(12, 23)
 	fmt.Println(c)
 	user := math.User{}
 	user.SetName("长位")
 	fmt.Println(user.Name)
 	fmt.Println("____________________")
-	fmt.Println(math.GetN())
+	fmt.Println(math.GetN())*/
+
+	//example MakeOrNew
+
 }
